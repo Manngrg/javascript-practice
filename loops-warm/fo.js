a2c99a979fb786b2929b14e812b14f31d41549e3
Manoj Gurung <manbgrg18@gmail.com>
2025-12-26 12:38:58 +0545

    basic css for amazon

---
+++ loops-warm/fo.js
@@ -0,0 +1,9 @@
+//loop that'll print out all even numbers between 10 and 40.
+
+// for(let i=10; i>=10 && i <=40; i=i+2){
+
+//     console.log(i);
+// }
+
+// create a variable "logged in" that starts off as false, create a forloop that loops while logged in is false , each loop prints "incorrect credentials", after 3 loops iteration change logged in to be true and after the loop is done , print out "succesfully logged in".
+
